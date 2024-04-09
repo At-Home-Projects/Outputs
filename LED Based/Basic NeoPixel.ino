949e2635fb33c54f4dcb7e466b34caadb7ff0773
Comrade-Bacon <120524043+Comrade-Bacon@users.noreply.github.com>
2024-04-09 10:53:16 -0600

    Update Basic NeoPixel.ino

--- LED Based/Basic NeoPixel.ino
+++ LED Based/Basic NeoPixel.ino
@@ -1,14 +1,15 @@
 
-
 /*
-*  rewriten from the simple example in the NeoPixel lybrary
-*  (Requiers Adafruit NeoPixel lybrary)
-*  (will work with all NeoPixles)
+*  Rewriten from the simple example in the NeoPixel lybrary
+*
+*  !!Requiers Adafruit NeoPixel lybrary!!
+*
+*  Should work with most if not NeoPixle strips, rings, ect...
 */
 
 #include <Adafruit_NeoPixel.h> // incude the NeoPixel lybrary
 
-int neoPin = 6; // Which pin on the Arduino is connected to the NeoPixels?
+int neoPin = 6;    // Which pin on the Arduino is connected to the NeoPixels?
 int numPixels = 8; // How many NeoPixels are there?
 
 Adafruit_NeoPixel pixels(numPixels, neoPin, NEO_GRB + NEO_KHZ800); // setup our strip
@@ -21,14 +22,14 @@ void loop() {
   pixels.clear(); // Set all pixels to off
 
   // turn all pixels on
-  for (int i = 0; i < numPixels; i ++) {  // The first NeoPixel in a strand is #0, second is 1, all the way up to the count of pixels minus one.
+  for (int i = 0; i < numPixels; i ++) {  // The first NeoPixel in a strand is 0. The second is 1, all the way up to the total number of pixels minus one.
 
     // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
     pixels.setPixelColor(i, pixels.Color(0, 50, 0)); // turn the given pixel on
 
     pixels.show();   // Send the updated pixel colors to the hardware.
     
-    // add a delay hear to see wich one lights up first!
+    delay(100);
   
   }
 
