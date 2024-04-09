d8b7f2c00cd1b8479db3bb180243c4917a531de4
Comrade-Bacon <120524043+Comrade-Bacon@users.noreply.github.com>
2024-04-09 10:45:43 -0600

    Update RGB LED.ino

--- LED Based/RGB LED.ino
+++ LED Based/RGB LED.ino
@@ -7,7 +7,7 @@ int blue = 9;
 
 void setup() {
 
-// setup rgb pins as output
+// setup RGB pins as output
 pinMode(red, OUTPUT);
 pinMode(green, OUTPUT);
 pinMode(blue, OUTPUT);
@@ -16,38 +16,38 @@ pinMode(blue, OUTPUT);
 
 void loop() {
 
-rgb(255,0,100); // call rgb function and trun it red
-delay(2000); // wait 2 sec
+rgb(255,0,100); // call RGB function and trun it red
+delay(2000);    // wait 2 sec
 
-rgb(0,0,0); // trun the RGB off
+rgb(0,0,0);  // trun the RGB off
 delay(2000); // wiat 2 sec
 
 }
 
 void rgb(int r,int g,int b) { // setup function
 
-  analogWrite(red, r); // red pin
+  analogWrite(red, r);   // red pin
   analogWrite(green, g); // green pin
-  analogWrite(blue, b); // blue pin
+  analogWrite(blue, b);  // blue pin
   
 }
 
-
-
-
-/* use this in void loop if you do not want to use a function
-
-// turn the RGB on
-analogWrite(red, 255); // trun red on as bright as it can be
-analogWrite(green, 0); // turn green off
-analogWrite(blue, 100); // turn blue on at 40% power
-
-delay(3000); // wait 3 sec
-
-// turn the RGB off
-analogWrite(red, 0); // trun red off
-analogWrite(green, 0); // trun green off
-analogWrite(blue, 0); // turn blue off
-
-delay(500); // wait 1 sec
+/*
+use the folowing alternate void loop if you would not like to use a function
 */
+
+void loop() {
+  // turn the RGB on
+  analogWrite(red, 255);  // trun red on as bright as it can be
+  analogWrite(green, 0);  // turn green off
+  analogWrite(blue, 100); // turn blue on at 40% power
+  
+  delay(3000); // wait 3 sec
+  
+  // turn the RGB off
+  analogWrite(red, 0);   // trun red off
+  analogWrite(green, 0); // trun green off
+  analogWrite(blue, 0);  // turn blue off
+  
+  delay(500); // wait 1/2 sec
+}
