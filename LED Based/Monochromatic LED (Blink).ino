68b21b191c5f26fafb68d747a84a811d2a8dbe77
Comrade-Bacon <120524043+Comrade-Bacon@users.noreply.github.com>
2024-04-09 11:40:23 -0600

    Update Monochromatic LED (Blink).ino

--- LED Based/Monochromatic LED (Blink).ino
+++ LED Based/Monochromatic LED (Blink).ino
@@ -7,7 +7,7 @@ void setup() {
 
 void loop() {
   digitalWrite(ledPin, HIGH);  // turn the LED on
-  delay(1000);                      // wait for a second
+  delay(1000);                 // wait for a second
   digitalWrite(ledPin, LOW);   // turn the LED off
-  delay(1000);                      // wait for a second
+  delay(1000);                 // wait for a second
 }
